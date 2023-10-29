241ee1e82d8eb2d5fe3f074a295736a57e7ba0a8
30Piraten <kioyarautenberg@gmail.com>
2023-10-29 19:11:15 +0200

    updated base.html

--- demo/urls.py
+++ demo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DemoViews, DemoPageView
+from .views import DemoViews, DemoPageView, DemoFormView
 
 urlpatterns = [
+    path("post/new", DemoFormView.as_view(), name="new_post"),
     path("post/<int:pk>/", DemoPageView.as_view(), name="post_detail"),
     path("", DemoViews.as_view(), name="home"),
 ]
