5c78d0215b915fcc5df84b1bdc2925ddd9a30d7b
30Piraten <kioyarautenberg@gmail.com>
2023-10-29 19:40:17 +0200

    updated demo/views and urls

--- demo/urls.py
+++ demo/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import DemoViews, DemoPageView, DemoFormView
+from .views import (
+    DemoViews, DemoPageView, DemoFormView, DemoUpdateView, DemoDeleteView
+)
 
 urlpatterns = [
+    path("post/<int:pk>/delete", DemoDeleteView.as_view(), name="delete_post"),
+    path("post/<int:pk>/edit/", DemoUpdateView.as_view(), name="edit_post"),
     path("post/new", DemoFormView.as_view(), name="new_post"),
     path("post/<int:pk>/", DemoPageView.as_view(), name="post_detail"),
     path("", DemoViews.as_view(), name="home"),
--- demo/views.py
+++ demo/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
@@ -12,10 +13,22 @@ class DemoViews(ListView):
 
 class DemoPageView(DetailView):
     model = Post
-    template_name = "post_details.html"
+    template_name = "post_detail.html"
 
 
 class DemoFormView(CreateView):
     model = Post
     template_name = "new_post.html"
     fields = ["title", "author", "body"]
+
+
+class DemoUpdateView(UpdateView):
+    model = Post
+    template_name = "edit_post.html"
+    fields = ["title", "body"]
+
+
+class DemoDeleteView(DeleteView):
+    model = Post
+    template_name = "delete_post.html"
+    success_url = reverse_lazy("home")
